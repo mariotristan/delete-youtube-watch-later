dc1420aff04e4cf6d79be29526ad695eff45456d
Mario Tristan <mariotristan@gmail.com>
2022-07-07 18:23:51 -0500

    Update test.js

--- test.js
+++ test.js
@@ -7,3 +7,12 @@ setInterval(function() {
 		}		
 }, 1000);
 
+
+----
+setInterval(function() {
+	document.querySelector('#contents button[aria-label="Menú de acciones"]').click();
+	var things = document.evaluate('//span[contains(text(),"Ver más tarde")]',document,null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,null);
+		for (var i = 0; i < things.snapshotLength; i++) {
+		    things.snapshotItem(i).click();
+		}		
+}, 1000);
